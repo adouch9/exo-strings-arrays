189c7b9bc889c4b0316b66afa24c672d3620d568
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-17 17:43:45 +0100

    FindBob

---
+++ FindBob.js
@@ -0,0 +1,5 @@
+let msg = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
+
+msg = msg.toUpperCase().trim()
+
+console.log(msg.includes('BOB')) ?  `L'occurence Bob est bien dans la phrase` : `L'occurence Bob n'est pas dans la phrase`
