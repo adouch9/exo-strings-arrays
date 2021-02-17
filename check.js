e64f9541bb8ec0f9ca1e6f0c1446a1afd809c528
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-17 15:57:38 +0100

    check

---
+++ check.js
@@ -0,0 +1,28 @@
+let text = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
+let javaScript = text.split(" ");
+let count = 0;
+
+
+for(let i = 0; i < text.length; i++)
+    if(javaScript[i] === 'Javascript')
+    count++
+    console.log("Le mot javascript est mentionner " + count + " fois dans le texte")
+
+
+
+
+
+
+
+
+// const msg = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
+// const mot = "Javascript";
+
+// console.log(`${msg.includes(mot) ? "Le mot javascript est bien présent" : "Le mot Javascript n'est pas présent"}`);bilaz666(Victor)Aujourd’hui à 15:10
+
+
+
+
+
+
+
