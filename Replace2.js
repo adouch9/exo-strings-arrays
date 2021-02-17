db55767c6fd68ccca0494c40aa8f4e95c1aea2af
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-17 16:49:32 +0100

    replace2

---
+++ Replace2.js
@@ -0,0 +1,10 @@
+
+let remplace = /Alice/gi;
+
+let text = "In cryptography, Alice and Bob are fictional characters commonly used as placeholders in discussions about cryptographic protocols or systems. \n\
+As the use of Alice and Bob became more widespread, additional characters were added, sometimes each with a particular meaning. \n\
+The most common characters are Alice and Bob. Eve, Mallory, and Trent are also common names.";
+
+const newText = text.replace(remplace, "Adel")
+
+console.log(newText);
