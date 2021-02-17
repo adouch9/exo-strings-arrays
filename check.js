382f8b4c8eca429f9b3a2598c2d928b2413f81e4
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-17 15:58:51 +0100

    check

--- check.js
+++ check.js
@@ -18,7 +18,7 @@ for(let i = 0; i < text.length; i++)
 // const msg = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
 // const mot = "Javascript";
 
-// console.log(`${msg.includes(mot) ? "Le mot javascript est bien présent" : "Le mot Javascript n'est pas présent"}`);bilaz666(Victor)Aujourd’hui à 15:10
+// console.log(`${msg.includes(mot) ? "Le mot javascript est bien présent" : "Le mot Javascript n'est pas présent"}`);
 
 
 
