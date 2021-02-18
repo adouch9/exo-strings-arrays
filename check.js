a6ef397f3f74f36d1da5d2551287aa3610c9b13b
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-18 15:48:16 +0100

    check

--- check.js
+++ check.js
@@ -1,18 +1,57 @@
-let text = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
-let javaScript = text.split(" ");
-let count = 0;
+const msg = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
+const mot = "Javascript";
+
+console.log(`${msg.includes(mot) ? "Le mot javascript est bien présent" : "Le mot Javascript n'est pas présent"}`);
 
 
-for(let i = 0; i < text.length; i++)
-    if(javaScript[i] === 'Javascript')
-    count++
-    console.log("Le mot javascript est mentionner " + count + " fois dans le texte")
 
 
-    // const msg = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
-// const mot = "Javascript";
+                        //    CHOIX : 2
 
-// console.log(`${msg.includes(mot) ? "Le mot javascript est bien présent" : "Le mot Javascript n'est pas présent"}`);
+// let text = "JS, ECMAScript, esm60 peuvent être considérés comme des alias de Javascript";       
+// let word = "Javascript";
+
+
+// console.log(`The word ${word} ${text.includes(word) ? "is" : "is not"} present`)
+
+// console.log(text)
+
+                       
+                             //  CHOIX : 3
+
+
+// let sentence = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript'
+// let word = 'JavaScript'
+// if (sentence.includes(word)) {
+//   console.log(`The word "${word}" is includes on the text :\n${sentence}`)
+// } else {
+//   console.log(`This text do not includes the word "${word}".`)
+// }
+
+                        //   CHOIX : 4 (TRUE AU MOT EXACTE AVEC LE NOMBRE MENTIONNER)
+
+
+// let text = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript";
+// let javaScript = text.split(" ");
+// let count = 0;
+
+
+// for(let i = 0; i < text.length; i++)
+//     if(javaScript[i] === 'Javascript')
+//     count++
+//     console.log("Le mot javascript est mentionner " + count + " fois dans le texte")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
