9a22d9fbd9fc1c022f410074e9130c6e466c3a0a
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-18 16:09:18 +0100

    Replace1

--- Replace1.js
+++ Replace1.js
@@ -1,9 +1,38 @@
-let prenomProf = "Sofiane";
-let monPrenom = "Adel";
+let text = "Bonjour, je suis Sofiane.";
+let myName = "Adel";
+
+text = text.replace("Sofiane", myName);
 
 
-prenomProf = monPrenom;
-console.log ("Bonjour, je suis " + prenomProf)
+console.log(text);
+
+
+        //  CHOIX 2 
+
+
+// let text = "Bonjour, je suis Sofiane.";
+
+// let myName = "Adel";
+
+// console.log(text.replace("Sofiane", myName));
+
+
+        //   CHOIX 3
+
+
+// let prenomProf = "Sofiane";
+// let monPrenom = "Adel";
+
+
+// prenomProf = monPrenom;
+// console.log ("Bonjour, je suis " + prenomProf)
+
+
+
+
+
+
+
 
 
 
